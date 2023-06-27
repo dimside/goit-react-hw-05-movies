ef82be69cd9dd379c7d2edd4e33b9718053e31e2
dimside <118112076+dimside@users.noreply.github.com>
2023-06-27 19:25:10 +0000

    Deploying to gh-pages from @ dimside/goit-react-hw-05-movies@90c3b760bcca49d5b9635a0d80ae609a0d417b2c 🚀

---
+++ static/js/856.6ab03097.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[856],{6856:function(t,e,a){a.d(e,{dWm:function(){return n}});var r=a(9983);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.41L6.83 13H21z"}}]})(t)}}}]);
+//# sourceMappingURL=856.6ab03097.chunk.js.map
