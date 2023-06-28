49ac210d2d07e259d4d8ceb05dca5aa831b54c4c
dimside <dimside29@gmail.com>
2023-06-28 15:25:04 +0300

    add-navigate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import SharedLayout from "pages/SharedLayout/SharedLayout"
 
@@ -14,12 +14,13 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />}></Route>
+          <Route path="movies" element={<Movies />}/>
           <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
+        <Route path="*" element={<Navigate to="/" replace/> } />
       </Routes>
     </>
   );
